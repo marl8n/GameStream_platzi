f9e86343d906c725bfb2a05762433f4e486dfeb7
marl8n <ivandmarlon@gmail.com>
2021-09-01 23:13:42 -0600

    Initial Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by Marlon De León on 9/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Marlon De León on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
